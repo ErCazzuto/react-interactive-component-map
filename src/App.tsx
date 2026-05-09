9feb77b1a787a435aea3dea979c82d3ee99301ae
ErCazzuto <ciagiao2023@gmail.com>
2026-05-09 18:45:52 +0200

    bicipite marasc

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ export default function App() {
             <p className="text-white-300 leading-relaxed">
               Nell' estate del 2024 sono, dopo anni che lo ambivo, finalmente riuscito ad assemblare il mio primo PC fisso. <br></br>
               Ho sempre avuto un computer portatile con prestazioni medie-basse, ma sono riuscito a sfruttare l'inizio della terza superiore e il fatto che avrei dovuto usare software come Solid Edge per usufruire delle ricerche che per pura passione svolgevo già da almeno 3 anni, e ho concretizzato il mio sogno: assemblarmi una bestia di PC. <br></br> 
-              <br></br>
+              
               In seguito parlerò di come ho scelto tutti i componenti, e di come ci ho messo mano assemblando il tutto, ma per ora vorrei fare una panoramica del mio PC e dei componenti descrivendone brevemente la funzione.
             </p>
           </div>
@@ -264,6 +264,126 @@ export default function App() {
         {/* Qui puoi aggiungere caselle di testo e immagini */}
         <div className="max-w-4xl mx-auto">
           <h2 className="text-3xl font-bold mt-8 mb-10 text-center"> <strong className="text-5xl text-[#4ade80]">Fase 2: l'assemblaggio</strong> </h2>
+        </div>
+      </section>
+
+      {/*Fase 1 assemblaggio*/}
+      <section className="p-8">
+        <div className="max-w-7xl mx-auto">
+          <div className="flex items-center">
+            <p className="text-white-30 leading-relaxed mb-8"> <strong className="text-3xl text-white">• Fase 1: preparazione dei componenti  </strong>
+            </p>
+          </div>
+          <div className="text-white-30 leading-relaxed mb-8">
+            Inanzitutto....
+          </div>
+          <div className="flex items-centered">
+          <img
+                  src="/fase1.jpg"
+                  alt="fase 1"
+                  className="w-6xl pr-1 h-full object-cover"
+          />
+          </div>
+        </div>
+      </section>
+
+      {/*Fase 2 assemblaggio*/}
+      <section className="p-8">
+        <div className="max-w-7xl mx-auto">
+          <div className="flex items-center">
+            <p className="text-white-30 leading-relaxed mb-8"> <strong className="text-3xl text-white">• Fase 2: sulla scheda madre...   </strong>
+            </p>
+          </div>
+          <div className="text-white-30 leading-relaxed mb-8">
+            Inanzitutto....
+          </div>
+          <div className="flex items-centered">
+          <img
+                  src="/fase2.jpg"
+                  alt="fase 2"
+                  className="w-6xl pr-1 h-full object-cover"
+          />
+          </div>
+        </div>
+      </section>
+
+      {/*Fase 3 assemblaggio*/}
+      <section className="p-8">
+        <div className="max-w-7xl mx-auto">
+          <div className="flex items-center">
+            <p className="text-white-30 leading-relaxed mb-8"> <strong className="text-3xl text-white">• Fase 3: nel case...   </strong>
+            </p>
+          </div>
+          <div className="text-white-30 leading-relaxed mb-8">
+            Inanzitutto....
+          </div>
+          <div className="flex items-centered">
+          <img
+                  src="/fase3.jpg"
+                  alt="fase 3"
+                  className="w-6xl pr-1 h-full object-cover"
+          />
+          </div>
+        </div>
+      </section>
+
+      {/*Fase 4 assemblaggio*/}
+      <section className="p-8">
+        <div className="max-w-7xl mx-auto">
+          <div className="flex items-center">
+            <p className="text-white-30 leading-relaxed mb-8"> <strong className="text-3xl text-white">• Fase 4: il terrore    </strong>
+            </p>
+          </div>
+          <div className="text-white-30 leading-relaxed mb-8">
+            purtroppo, è arrivato il momento
+          </div>
+          <div className="flex items-centered">
+          <img
+                  src="/fase4.jpg"
+                  alt="fase 4"
+                  className="w-6xl pr-1 h-full object-cover"
+          />
+          </div>
+        </div>
+      </section>
+
+      {/*Fase 5 assemblaggio*/}
+      <section className="p-8">
+        <div className="max-w-7xl mx-auto">
+          <div className="flex items-center">
+            <p className="text-white-30 leading-relaxed mb-8"> <strong className="text-3xl text-white">• Fase 5: la bestia  </strong>
+            </p>
+          </div>
+          <div className="text-white-30 leading-relaxed mb-8">
+            E dopo il terrore di prima, finalmente, è l'ora di montare la GPU!
+          </div>
+          <div className="flex items-centered">
+          <img
+                  src="/fase5.jpg"
+                  alt="fase 5"
+                  className="w-6xl pr-1 h-full object-cover"
+          />
+          </div>
+        </div>
+      </section>
+
+      {/*Fase 6 assemblaggio*/}
+      <section className="p-8">
+        <div className="max-w-7xl mx-auto">
+          <div className="flex items-center">
+            <p className="text-white-30 leading-relaxed mb-8"> <strong className="text-3xl text-white">• Fase 6: il PRIMO AVVIO   </strong>
+            </p>
+          </div>
+          <div className="text-white-30 leading-relaxed mb-8">
+            Inanzitutto....
+          </div>
+          <div className="flex items-centered">
+          <img
+                  src="/fase6.jpg"
+                  alt="fase 6"
+                  className="w-6xl pr-1 h-full object-cover"
+          />
+          </div>
         </div>
       </section>
 
